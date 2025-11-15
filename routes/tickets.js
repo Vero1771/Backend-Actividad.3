dd94588537c942e50ffe7134333c24d0d9ac68dd
Veronica Andrade <andradepenaveju@uvm.edu.ve>
2025-11-15 15:23:15 -0400

    Agregar vistas para tickets

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const TicketController = require('../controllers/ticketController');
+const VentaController = require('../controllers/ventaController');
+const FuncionController = require('../controllers/funcionController');
 
 router.get('/api', (req, res) => {
   TicketController.index()
@@ -46,3 +48,53 @@ router.get('/api/byVenta/:id', (req, res) => {
 });
 
 module.exports = router;
+
+// --- View routes (EJS) ---
+router.get('/', (req, res) => {
+  TicketController.index()
+    .then(tickets => res.render('tickets/index', { tickets }))
+    .catch(err => res.status(err.status || 500).send(err.message || err));
+});
+
+router.get('/new', (req, res) => {
+  const prefillVenta = req.query.id_venta;
+  Promise.all([VentaController.index(), FuncionController.index()])
+    .then(([ventas, funciones]) => res.render('tickets/new', { ventas, funciones, prefillVenta }))
+    .catch(err => res.status(err.status || 500).send(err.message || err));
+});
+
+router.post('/create', (req, res) => {
+  TicketController.create(req.body)
+    .then(() => res.redirect('/tickets'))
+    .catch(err => res.status(err.status || 500).send(err.message || err));
+});
+
+router.get('/:id/edit', (req, res) => {
+  Promise.all([TicketController.findById(req.params.id), VentaController.index(), FuncionController.index()])
+    .then(([ticket, ventas, funciones]) => {
+      if (!ticket) return res.status(404).send('Not found');
+      res.render('tickets/edit', { ticket, ventas, funciones });
+    })
+    .catch(err => res.status(err.status || 500).send(err.message || err));
+});
+
+router.post('/:id/update', (req, res) => {
+  TicketController.update(req.params.id, req.body)
+    .then(() => res.redirect('/tickets'))
+    .catch(err => res.status(err.status || 500).send(err.message || err));
+});
+
+router.post('/:id/delete', (req, res) => {
+  TicketController.delete(req.params.id)
+    .then(() => res.redirect('/tickets'))
+    .catch(err => res.status(err.status || 500).send(err.message || err));
+});
+
+router.get('/:id', (req, res) => {
+  TicketController.findById(req.params.id)
+    .then(ticket => {
+      if (!ticket) return res.status(404).send('Not found');
+      res.render('tickets/show', { ticket });
+    })
+    .catch(err => res.status(err.status || 500).send(err.message || err));
+});
